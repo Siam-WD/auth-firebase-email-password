71196e8361cb8084aa58303a06b825e8d4ee3e73
Siam-WD <abusayedalsiam2023@gmail.com>
2023-04-27 12:40:35 +0600

    sign in and register complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4XKU-JDXsdIxtG7yzAZdoltD0hCm9Wso",
+  authDomain: "auth-firebase-contex-tai-6f827.firebaseapp.com",
+  projectId: "auth-firebase-contex-tai-6f827",
+  storageBucket: "auth-firebase-contex-tai-6f827.appspot.com",
+  messagingSenderId: "790042336102",
+  appId: "1:790042336102:web:1c772e10b0ecfc051f86a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app ;
